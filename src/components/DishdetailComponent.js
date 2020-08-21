67cfc1372ea2d465c214d4dc4a2246ace0dbb489
Shubham B <shubham200015@yahoo.com>
2020-08-21 13:37:33 +0530

    Fetch Post Comment

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -44,7 +44,7 @@ function RenderDish({ dish }) {
 }
 
 // function to render all the comments corresponding to the selected dish
-function RenderComments({ comments, addComment, dishId }) {
+function RenderComments({ comments, postComment, dishId }) {
 	if (comments != null) {
 		const cmnts = comments.map((commnts) => {
 			return (
@@ -69,7 +69,7 @@ function RenderComments({ comments, addComment, dishId }) {
 			<div className="col-12 col-md-5 m-2">
 				<h4> Comments </h4>
 				{cmnts}
-				<CommentForm dishId={dishId} addComment={addComment} />
+				<CommentForm dishId={dishId} postComment={postComment} />
 			</div>
 		);
 		// if comments is empty
@@ -98,7 +98,7 @@ class CommentForm extends Component {
 
 	handleSubmit(values) {
 		this.toggleModal();
-		this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
+		this.props.postComment(this.props.dishId, values.rating, values.author, values.comment);
 	}
 
 	render() {
@@ -213,7 +213,7 @@ const DishDetail = (props) => {
 				</div>
 				<div className="row">
 					<RenderDish dish={props.dish} />
-					<RenderComments comments={props.comments} addComment={props.addComment} dishId={props.dish.id} />
+					<RenderComments comments={props.comments} postComment={props.postComment} dishId={props.dish.id} />
 				</div>
 			</div>
 		);
